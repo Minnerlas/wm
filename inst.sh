5c6f205330c7ce540ab6ba42994ff1ad24714e80
Minnerlas <nikilirikili@gmail.com>
2020-06-29 21:16:16 +0200

    test

--- inst.sh
+++ inst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IME=testime
-SUDOERS=sudoers
+SUDOERS=/etc/sudoers
 SHELL=/bin/zsh
 
 useradd $IME -s $SHELL -m
@@ -11,8 +11,27 @@ echo $IME "ALL=(ALL) NOPASSWD:ALL" >> $SUDOERS
 
 su $IME
 
+sudo pacman -S base-devel
+
+git clone https://github.com/minnerlas/tackice
 
 
+git clone https://github.com/minnerlas/tackice
+cp -r tackice/* .
+rm -rvf .git 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+# git clone https://github.com/minnerlas/wm
+# cd
+# instalirati bitne pakete
+# wm/skripte/instpakete.sh
+# ln ~/wm/razno/dwm.desktop /usr/share/xessions/dwm.desktop
+# podesiti /etc/lightdm/lightdm.conf ([Seat] user-session = dwm)
+# sudo systemctl enable lightdm.service
+# kopirati pozadine
+
+exit
 
 # mv $SUDOERS.bak $SUDOERS
 # echo $IME "ALL = NOPASSWD: /bin/systemctl restart httpd.service, /bin/kill" >> $SUDOERS
